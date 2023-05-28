a5073189314fd66ed2685e5459185b32a112c603
jebushdd <jebushdd@gmail.com>
2023-05-28 17:50:45 -0300

    Se agrega la etapa de transformacion

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from extraer_datos import get_urls, descargar_csv
 from rutas import crear_dir, DIR_BIBLIOTECAS, DIR_CINES, DIR_MUSEOS, CSV_BIBLIOTECAS, CSV_CINES, CSV_MUSEOS
+from transformar_datos import normalizar_campos, nuevos_campos, rename_cols
 
 
 if __name__ == '__main__':
+
+    # -------- Parte 1: Extracción -------- #
+    # ------------------------------------- #
 
     # Definir URL principal y crear diccionario de URLs por base de datos
     url = 'https://datos.cultura.gob.ar/dataset/espacios-culturales-argentina-sinca'
@@ -22,3 +26,21 @@ if __name__ == '__main__':
     descargar_csv(url_bibliotecas, CSV_BIBLIOTECAS)
     descargar_csv(url_museos, CSV_MUSEOS)
     descargar_csv(url_cines, CSV_CINES)
+
+    # -------- Parte 2: Transformación -------- #
+    # ----------------------------------------- #
+
+    # Cargar csvs y normalizar campos
+    raw_df_bibliotecas = normalizar_campos(CSV_BIBLIOTECAS)
+    raw_df_museos = normalizar_campos(CSV_MUSEOS)
+    raw_df_cines = normalizar_campos(CSV_CINES)
+
+    # Tomar campos requeridos de cada dataframe
+    df_bibliotecas = nuevos_campos(raw_df_bibliotecas)
+    df_museos = nuevos_campos(raw_df_museos)
+    df_cines = nuevos_campos(raw_df_cines)
+
+    # Crear dataframes con columnas renombradas
+    df_bibliotecas = rename_cols(df_bibliotecas)
+    df_museos = rename_cols(df_museos)
+    df_cines = rename_cols(df_cines)
