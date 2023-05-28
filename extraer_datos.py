f3349831f22222c87782536c6334b6f61c8bcc73
jebushdd <jebushdd@gmail.com>
2023-05-28 13:38:57 -0300

    modularizado script extraccion. Creado main.py

--- extraer_datos.py
+++ extraer_datos.py
@@ -3,13 +3,11 @@ import requests
 import os
 from datetime import date
 
-url = 'https://datos.cultura.gob.ar/dataset/espacios-culturales-argentina-sinca'
 
-
-def get_urls(url):
+def get_urls(url:str):
     '''
-    Devuelve un diccionario con las urls de los recursos del dataset
-    Pasar como argumento la url del dataset
+    Devuelve un diccionario con las urls de los recursos del dataset.
+    Pasar como argumento la url del dataset.
     '''
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'lxml')
@@ -19,52 +17,47 @@ def get_urls(url):
                              .find_previous('button', string='DESCARGAR')\
                              .find_previous('a')['href']
     return diccionario
-dataset = get_urls(url)
 
-# Definir strings de fechas
-anio_mes = date.today().strftime('%Y-%B').lower()
-dia_mes_anio = date.today().strftime('%d-%m-%Y')
 
-# Definir directorios
-dir_actual = os.getcwd()
-dir_bibliotecas = os.path.join(dir_actual+'/bibliotecas/'+anio_mes)
-dir_museos = os.path.join(dir_actual+'/museos/'+anio_mes)
-dir_cines = os.path.join(dir_actual+'/cines/'+anio_mes)
-
-# Creando directorios
-def crear_dir(dir_categoria):
+def crear_str_dir(nombre_categoria:str):
     '''
-    Crea el directorio con las rutas especificadas para cada categoria
-    Pasar una ruta (dir_categoria) por vez
+    Devuelve el directorio donde se guarda el oputput.
+    Toma como argumento un string con el nombre de la categoría del output.
+    '''
+    # Definir string año-mes
+    anio_mes = date.today().strftime('%Y-%B').lower()
+     
+    # Definir directorio
+    dir_actual = os.getcwd()
+    dir_output = os.path.join(f'{dir_actual}/{nombre_categoria}/{anio_mes}')
+    return dir_output
+
+
+def crear_dir(dir_categoria:str):
+    '''
+    Crea el directorio con las rutas especificadas para cada categoria.
+    Toma como argumento una ruta (dir_categoria).
     '''
     if not os.path.exists(dir_categoria):
             os.makedirs(dir_categoria)
 
-crear_dir(dir_bibliotecas)
-crear_dir(dir_cines)
-crear_dir(dir_museos)
 
-# Definir urls para cada base de datos
-url_bibliotecas = dataset.get('Bibliotecas Populares')
-url_museos = dataset.get('Museo')
-url_cines = dataset.get('Cine')
-
-# Definir path para archivos descargados
-csv_bibliotecas = os.path.join(dir_bibliotecas+'/bibliotecas-'+dia_mes_anio+'.csv')
-csv_museos = os.path.join(dir_museos+'/museos-'+dia_mes_anio+'.csv')
-csv_cines = os.path.join(dir_cines+'/cines-'+dia_mes_anio+'.csv')
-
-# Descargando csvs
-def descargar_csv(url_csv, dir_salida):
+def crear_dir_output(dir_categoria:str, nombre_categoria:str):
     '''
-    Descarga el csv de la categoria correspondiente en la ruta especificada
+    Genera un string con la ruta del output en formato csv.
+    Toma como argumento un string con el nombre de la categoría.
+    '''
+    dia_mes_anio = date.today().strftime('%d-%m-%Y')
+    output_csv = os.path.join(f'{dir_categoria}/{nombre_categoria}-{dia_mes_anio}.csv')
+    return output_csv
+
+
+def descargar_csv(url_csv:str, dir_salida:str):
+    '''
+    Descarga el csv de la categoria correspondiente en la ruta especificada.
     Pasar como primer argumento la url al csv y como segundo argumento la ruta
     completa incluyendo el nombre del archivo de salida
     '''
     response = requests.get(url_csv)
     with open(dir_salida, 'wb') as csv:
         csv.write(response.content)
-
-descargar_csv(url_bibliotecas, csv_bibliotecas)
-descargar_csv(url_museos, csv_museos)
-descargar_csv(url_cines, csv_cines)
