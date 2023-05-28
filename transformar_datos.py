30b980eb7fccc1336658be34b1e610f049027aea
jebushdd <jebushdd@gmail.com>
2023-05-28 17:47:49 -0300

    Se modularizo el proceso de transformacion de datos

--- transformar_datos.py
+++ transformar_datos.py
@@ -1,80 +1,69 @@
 import pandas as pd
-import os
-from datetime import date
 
-# ❕❕❕ Crear un constants para las strings de fechas y paths ❕❕❕
+from rutas import CSV_BIBLIOTECAS, CSV_CINES, CSV_MUSEOS
 
-# Paths para csvs
-# Definir strings de fechas
-anio_mes = date.today().strftime('%Y-%B')
-dia_mes_anio = date.today().strftime('%d-%m-%Y')
 
-# Definir directorios
-dir_actual = os.getcwd()
-dir_bibliotecas = os.path.join(dir_actual+'/bibliotecas/'+anio_mes)
-dir_museos = os.path.join(dir_actual+'/museos/'+anio_mes)
-dir_cines = os.path.join(dir_actual+'/cines/'+anio_mes)
+def normalizar_campos(csv_file:str):
+    # Leer csv
+    raw_df = pd.read_csv(csv_file)
+    # Normalizar campos y datos
+    raw_df.columns = raw_df.columns.str.lower()
+    raw_df.columns = raw_df.columns.str.normalize('NFKD').str.encode('ascii',errors='ignore').str.decode('utf-8')
+    raw_df.fillna('s/d', inplace=True)
+    return raw_df
 
-# Definir path de archivos descargados
-csv_bibliotecas = os.path.join(dir_bibliotecas+'/bibliotecas-'+dia_mes_anio+'.csv')
-csv_museos = os.path.join(dir_museos+'/museos-'+dia_mes_anio+'.csv')
-csv_cines = os.path.join(dir_cines+'/cines-'+dia_mes_anio+'.csv')
+def nuevos_campos(df:pd.DataFrame):
+    match df.categoria[0]:
+        case 'Salas de cine':
+            df[['número de teléfono', 'mail']] = 's/d'
+            new_df = df[['cod_localidad', 'id_provincia', 'id_departamento', 'categoria', 'provincia', 'localidad', 'nombre', 'direccion', 'cp', 'número de teléfono','mail' , 'web']]
+        
+        case 'Espacios de Exhibición Patrimonial':
+            new_df = df[['cod_loc', 'idprovincia', 'iddepartamento', 'categoria', 'provincia', 'localidad', 'nombre', 'direccion', 'cp', 'telefono', 'mail', 'web']]
+        
+        case 'Bibliotecas Populares':
+            new_df = df[['cod_loc', 'idprovincia', 'iddepartamento', 'categoria', 'provincia', 'localidad', 'nombre', 'domicilio', 'cp', 'telefono', 'mail', 'web']]
+    
+    return new_df
 
-# Leer csvs
-raw_df_bibliotecas = pd.read_csv(csv_bibliotecas)
-raw_df_museos = pd.read_csv(csv_museos)
-raw_df_cines = pd.read_csv(csv_cines)
-
-# Pre procesar nombres de variables
-def set_columns_lower(df):
-    '''
-    Convierte los nombres de las variables
-    del dataframe a minúsculas
-    '''
-    df.columns = df.columns.str.lower()
-
-set_columns_lower(raw_df_bibliotecas)
-set_columns_lower(raw_df_museos)
-set_columns_lower(raw_df_cines)
-
-# Crear lista de nomenclaturas para nueva tabla
-columnas = ['cod_localidad', 'id_provincia', 'id_departamento', 'categoría', 
-            'provincia', 'localidad', 'nombre', 'domicilio', 'código postal', 
-            'número de teléfono', 'mail', 'web']
-
-# Seleccionar columnas requeridas en cada dataframe
-df_bibliotecas = raw_df_bibliotecas[['cod_loc', 'idprovincia', 'iddepartamento', 'categoría', 'provincia', 'localidad', 'nombre', 'domicilio', 'cp', 'teléfono', 'mail', 'web']]
-df_museos = raw_df_museos[['cod_loc', 'idprovincia', 'iddepartamento', 'categoria', 'provincia', 'localidad', 'nombre', 'direccion', 'cp', 'telefono', 'mail', 'web']]
-
-    # Museos necesita columnas para telefono y mail
-raw_df_cines[['número de teléfono', 'mail']] = 's/d'
-df_cines = raw_df_cines[['cod_localidad', 'id_provincia', 'id_departamento', 'categoria', 'provincia', 'localidad', 'nombre', 'direccion', 'cp', 'número de teléfono','mail' , 'web']]
-
-# Renombrando columnas
-def rename_cols(df, new_names):
+def rename_cols(df:pd.DataFrame):
     '''
     Crea un diccionario a partir de un zip entre las columnas actuales 
-    del dataframe y una lista con sus correspondientes nuevos nombres
-    Pasar como primer argumento el dataframe con las columnas a renombrar
-    Pasar como segundo argumento la lista con los nuevos nombres de las columnas
-    Los indices de ambos argumentos deben coincidir para que se descompaginen los nombres
+    del dataframe y una lista con los correspondientes nuevos nombres.
+    Pasar como argumento el dataframe con las columnas a renombrar.
     '''
+    # Crear lista de campos para nueva tabla
+    new_names = ['cod_localidad', 'id_provincia', 'id_departamento', 'categoría', 
+                'provincia', 'localidad', 'nombre', 'domicilio', 'código postal', 
+                'número de teléfono', 'mail', 'web']
+    
     diccionario = dict(zip(df.columns, new_names))
     df = df.rename(columns=diccionario)
     return df
 
-# Crear dataframes con columnas renombradas
-df_bibliotecas = rename_cols(df_bibliotecas, columnas)
-df_museos = rename_cols(df_museos, columnas)
-df_cines = rename_cols(df_cines, columnas)
+def unir_dfs(lista_dfs:list):
+    consolidado = pd.concat(lista_dfs, ignore_index=True)
+    return consolidado
 
-# Normalizando datos
-df_bibliotecas.fillna('s/d', inplace=True)
-df_museos.fillna('s/d', inplace=True)
-df_cines.fillna('s/d', inplace=True)
 
+# Leer csvs y normalizar campos
+raw_df_bibliotecas = normalizar_campos(CSV_BIBLIOTECAS)
+raw_df_museos = normalizar_campos(CSV_MUSEOS)
+raw_df_cines = normalizar_campos(CSV_CINES)
+# Tomar campos requeridas en cada dataframe
+df_bibliotecas = nuevos_campos(raw_df_bibliotecas)
+df_museos = nuevos_campos(raw_df_museos)
+df_cines = nuevos_campos(raw_df_cines)
+# Crear dataframes con campos renombrados
+df_bibliotecas = rename_cols(df_bibliotecas)
+df_museos = rename_cols(df_museos)
+df_cines = rename_cols(df_cines)
 # Crear tabla unica
-consolidado = pd.concat([df_bibliotecas,df_museos,df_cines], ignore_index=True)
+lista_dfs = [df_bibliotecas,df_museos,df_cines]
+consolidado = unir_dfs(lista_dfs)
+
+
+
 
 # Registros totales por categoria
 registros_categoria = pd.DataFrame(consolidado.groupby(by= 'categoría')['categoría'].count())
