a1312249d968dc0f71d270e37a6f911692d518ff
jebushdd <jebushdd@gmail.com>
2023-05-29 01:56:26 -0300

    Agregados calculos de tablas. Deberian tener su propio modulo

--- transformar_datos.py
+++ transformar_datos.py
@@ -1,22 +1,51 @@
+import unicodedata
 import pandas as pd
 
 from rutas import CSV_BIBLIOTECAS, CSV_CINES, CSV_MUSEOS
 
 
-def normalizar_campos(csv_file:str):
+def normalizar_csv(csv_file:str):
+    '''
+    Carga un archivo csv como DataFrame y normaliza tanto sus campos
+    como sus valores tipo string.
+    Toma como argumento un string con la ruta al CSV
+    '''
     # Leer csv
     raw_df = pd.read_csv(csv_file)
-    # Normalizar campos y datos
+    # Normalizar campos
     raw_df.columns = raw_df.columns.str.lower()
     raw_df.columns = raw_df.columns.str.normalize('NFKD').str.encode('ascii',errors='ignore').str.decode('utf-8')
+    
+    # Normalizar datos
+    raw_df = raw_df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
     raw_df.fillna('s/d', inplace=True)
-    return raw_df
+
+    def no_tildes(texto:str):
+        '''
+        Quita las tildes de un string. Toma como argumento un string.
+        '''
+        new_value = unicodedata.normalize('NFKD',texto).encode('ascii',errors='ignore').decode('utf-8')
+        return new_value
+    
+    new_df = raw_df.applymap(lambda x: no_tildes(x) if isinstance(x, str) else x)
+    
+    # Unificar Tierra Del Fuego
+    def limpiar_tdf(provincia:pd.Series):
+        '''
+        Quita el exceso de texto para 'Tierra del Fuego' en el campo 'Provincia'.
+        Toma como argumento un objeto Pandas Series.
+        '''
+        return provincia.split(',')[0]
+
+    new_df['provincia'] = new_df['provincia'].apply(limpiar_tdf)
+    return new_df
 
 def nuevos_campos(df:pd.DataFrame):
     match df.categoria[0]:
         case 'Salas de cine':
-            df[['número de teléfono', 'mail']] = 's/d'
-            new_df = df[['cod_localidad', 'id_provincia', 'id_departamento', 'categoria', 'provincia', 'localidad', 'nombre', 'direccion', 'cp', 'número de teléfono','mail' , 'web']]
+            new_df = df[['cod_localidad', 'id_provincia', 'id_departamento', 'categoria', 'provincia', 'localidad', 'nombre', 'direccion', 'cp']].copy()
+            new_df[['número de teléfono', 'mail']] = 's/d'
+            new_df['web'] = df['web']
         
         case 'Espacios de Exhibición Patrimonial':
             new_df = df[['cod_loc', 'idprovincia', 'iddepartamento', 'categoria', 'provincia', 'localidad', 'nombre', 'direccion', 'cp', 'telefono', 'mail', 'web']]
@@ -41,15 +70,22 @@ def rename_cols(df:pd.DataFrame):
     df = df.rename(columns=diccionario)
     return df
 
-def unir_dfs(lista_dfs:list):
+def tabla_unificada(lista_dfs:list):
+    '''
+    Concatena varios DataFrames y devuelve uno unificado. 
+    Toma como argumento una lista de DataFrames.
+    '''
     consolidado = pd.concat(lista_dfs, ignore_index=True)
     return consolidado
 
+def tabla_categorias():
+    return ''
+
 
 # Leer csvs y normalizar campos
-raw_df_bibliotecas = normalizar_campos(CSV_BIBLIOTECAS)
-raw_df_museos = normalizar_campos(CSV_MUSEOS)
-raw_df_cines = normalizar_campos(CSV_CINES)
+raw_df_bibliotecas = normalizar_csv(CSV_BIBLIOTECAS)
+raw_df_museos = normalizar_csv(CSV_MUSEOS)
+raw_df_cines = normalizar_csv(CSV_CINES)
 # Tomar campos requeridas en cada dataframe
 df_bibliotecas = nuevos_campos(raw_df_bibliotecas)
 df_museos = nuevos_campos(raw_df_museos)
@@ -58,10 +94,10 @@ df_cines = nuevos_campos(raw_df_cines)
 df_bibliotecas = rename_cols(df_bibliotecas)
 df_museos = rename_cols(df_museos)
 df_cines = rename_cols(df_cines)
-# Crear tabla unica
+# Crear tabla unificada
 lista_dfs = [df_bibliotecas,df_museos,df_cines]
-consolidado = unir_dfs(lista_dfs)
-
+consolidado = tabla_unificada(lista_dfs)
+# Crear tabla de categorías
 
 
 
